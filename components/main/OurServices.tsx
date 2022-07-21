77998425a872a21b0f75fdf10e3fee4503273409
anthonybartczak <to0oni456@gmail.com>
2022-07-21 20:52:11 +0200

    Tailwind readjustments

---
+++ components/main/OurServices.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function OurServices() {
+  return (
+  <section id="our-services" className="scroll-m-20">
+      <div className="max-w-screen-xl mt-64 px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <h2 className="text-4xl text-center tracking-tight font-bold text-gray-900 sm:text-5xl md:text-4xl">
+          <span className="block xl:inline underline underline-offset-8 decoration-indigo-600">Nasze usługi</span>
+        </h2>
+        <div className="grid grid-cols-1 mt-8 justify-items-stretch gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <a className="block p-8 transition border border-gray-400 shadow-xl rounded-xl hover:shadow-indigo-500/20 hover:border-indigo-500" href="/services/digital-campaigns">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+            </svg>
+
+            <h3 className="mt-4 text-xl font-bold">Diagnostyka i serwis</h3>
+
+            <p className="mt-1 text-sm text-gray-500">
+            Świadczymy usługi naprawcze obejmujące wszystkie elementy mechaniki samochodowej dla wszelkich modeli pojazdów każdego z producentów samochodowych.
+            </p>
+          </a>
+
+          <a className="block p-8 transition border border-gray-400 shadow-xl rounded-xl hover:shadow-indigo-500/20 hover:border-indigo-500" href="/services/digital-campaigns">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+            </svg>
+
+            <h3 className="mt-4 text-xl font-bold">Obsługa flot</h3>
+
+            <p className="mt-1 text-sm text-gray-500">
+            Naszym nadrzędnym celem jest zapewnienie profesjonalnej obsługi flot samochodowych obejmującej wszelkie aspekty serwisu eksploatacyjnego pojazdu.
+            </p>
+          </a>
+
+          <a className="block p-8 transition border border-gray-400 shadow-xl rounded-xl hover:shadow-indigo-500/20 hover:border-indigo-500" href="/services/digital-campaigns">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+            </svg>
+
+            <h3 className="mt-4 text-xl font-bold">Budowa aut na zamówienie</h3>
+
+            <p className="mt-1 text-sm text-gray-500">
+            Zajmujemy się budową samochodów terenowych czy driftowozów od podstaw jak również realizujemy tunning i przygotowanie samochodów cywilnych do rajdów i wyścigów.
+            </p>
+          </a>
+
+          <a className="block p-8 transition border border-gray-400 shadow-xl rounded-xl hover:shadow-indigo-500/20 hover:border-indigo-500" href="/services/digital-campaigns">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+            </svg>
+
+            <h3 className="mt-4 text-xl font-bold">Stacja kontroli pojazdów</h3>
+
+            <p className="mt-1 text-sm text-gray-500">
+            Wykonujemy badania techniczne samochodów osobowych i ciężarowych o dopuszczalnej masie całkowitej do 3,5 t oraz motocykli i pojazdów z instalacją gazową.
+            </p>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+      },
+      minWidth: {
+        '24': '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
