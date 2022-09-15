72659a2078420b5b9004fdddb9fbb5d93bc3797f
anthonybartczak <to0oni456@gmail.com>
2022-09-15 10:54:39 +0200

    Fixes for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
-import { GetBearerToken } from "../utils/mutations";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
