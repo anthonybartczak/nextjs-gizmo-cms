bb759cfe146eb64f91888cb5c698d73e36014c8b
anthonybartczak <to0oni456@gmail.com>
2022-10-03 13:54:12 +0200

    Added Piwik PRO container code

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FaInstagramSquare, FaFacebookSquare } from "react-icons/fa";
 
 export const Hero = () => (
   <div className="hero grid xl:grid-cols-2 pb-8 relative">
@@ -17,7 +16,7 @@ export const Hero = () => (
         <h1 className="text-gray-100 text-5xl font-extrabold">
           Management zrobiony
         </h1>
-        <h1 className="text-5xl font-extrabold text-rose-600">dobrze.</h1>
+        <h1 className="text-5xl font-bold text-rose-600">dobrze.</h1>
         <p className="text-gray-200 py-5 max-w-2xl">
           Fusce sed lacus ut dolor consequat euismod. Quisque non eros mollis,
           condimentum purus vitae, bibendum quam. In urna leo, facilisis quis
@@ -30,7 +29,7 @@ export const Hero = () => (
             <button className="relative p-3 group">
               <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 group-hover:bg-rose-600 bg-gray-100 group-hover:-translate-x-0 group-hover:-translate-y-0" />
               <span className="absolute inset-0 w-full h-full bg-black border-2 border-gray-100 group-hover:bg-black group-hover:border-rose-600" />
-              <span className="relative text-gray-100 text-xl uppercase group-hover:text-rose-600">
+              <span className="relative text-gray-100 text-xl uppercase ">
                 Skontaktuj się z nami!
               </span>
             </button>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,20 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="bg-gray-100">
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(window, document, dataLayerName, id) {
+              window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
+              function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString()}document.cookie=a+"="+b+d+"; path=/"}
+              var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
+              var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
+              tags.async=!0,tags.src="https://abartczak.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
+              !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
+              })(window, document, 'dataLayer', '86cffefb-8308-46b1-8251-e5803d7503ea');
+              `,
+          }}
+        ></script>
         <Main></Main>
         <NextScript />
       </body>
